bfe7db41d0ac89ed615710851587cee777887ef2
lcfd <9001053+fedriz@users.noreply.github.com>
2023-05-31 09:56:42 +0200

    Fix image path using Image component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://pythonitalia.github.io",
   base: "/trento.python.it",
-  integrations: [tailwind({ config: { applyBaseStyles: false } })],
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    image({ serviceEntryPoint: "@astrojs/image/sharp" }),
+  ],
 });
